94399842fa309911013658ce5ebe406fc43daf1a
aishwarya134 <112769433+aishwarya134@users.noreply.github.com>
2022-09-27 22:21:34 +0530

    Create CastingExercise.java

---
+++ CastingExercise.java
@@ -0,0 +1,11 @@
+public class CastingExercise { 
+public static void main(String[] args) { 
+byte b=10; 
+int i=b;
+byte c=i;r
+byte d=(byte)i;
+  byte x=10;
+  byte y=20; 
+  byte sum=x*y;  
+ } 
+} 
