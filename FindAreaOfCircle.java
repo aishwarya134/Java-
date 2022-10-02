ec6d382cdd9fe09307125e5af8b3126359257390
aishwarya134 <112769433+aishwarya134@users.noreply.github.com>
2022-10-02 20:23:44 +0530

    Create FindAreaOfCircle.java

---
+++ FindAreaOfCircle.java
@@ -0,0 +1,16 @@
+class FindAreaOfCircle 
+{ 
+public static void main(String args[]) 
+ { 
+ Scanner scanner = new Scanner(System.in); 
+ System.out.print("Enter the radius: "); 
+ double radius = scanner.nextDouble(); 
+ //Area = PI*radius*radius
+double area = Math.PI * (radius * radius); 
+ System.out.println("The area of circle is: " + area); 
+ //Circumference = 2*PI*radius
+double circumference= Math.PI * 2*radius; 
+ System.out.println( "The circumference of the circle 
+is:"+circumference) ; 
+ } 
+}
